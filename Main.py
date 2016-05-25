0a6dea383abd79fa84b96040c2ea99eeb210e08e
patnir <rahul20patni@gmail.com>
2016-05-25 12:19:41 -0400

    Modifications - figuring out data structure

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ Created on Tue May 24 19:59:21 2016
 @author: Rahul Patni
 """
 import csv
-from numpy import matrix
 
 def openingMessage():
     print("\n" + "My attempt at housing price prediction from a dataset of 50 houses" + "\n")
@@ -21,8 +20,8 @@ def assignHeading(data):
     for i in range(0,len(data[0])):
         heading.append(data[0][i])    
     return heading
-
-def print2dArray(array):
+    
+def printArray(array):
     for i in range(0, len(array)):
         print i
         print array[i]
@@ -32,9 +31,11 @@ def main():
     data = importData("data.csv")
     heading = assignHeading(data)
     print heading
-    
-    X = matrix(data)
-    print2dArray(X)
+    y = [[each_list[i] for i in range(len(data[0]) - 1, len(data[0]))] for each_list in data[1:len(data)]]
+    printArray(y)   
+    X = [[each_list[i] for i in range(0, len(data[0]) - 1)] for each_list in data[1:len(data)]]
+    printArray(X)
+    #printArray(y)
             
 if __name__ == "__main__":
     main()
