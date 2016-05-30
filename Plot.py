033c4c4ac011177f1dac5304bb2eabb9392df63b
patnir <rahul20patni@gmail.com>
2016-05-30 11:02:53 -0400

    Add plot implementation

--- Plot.py
+++ Plot.py
@@ -5,6 +5,8 @@ Created on Mon May 30 10:46:07 2016
 @author: Rahul Patni
 """
 import csv
+import matplotlib.pyplot as plt
+
 
 def ImportData(filename):
     with open(filename, 'rb') as csvfile:
@@ -14,9 +16,15 @@ def ImportData(filename):
             data[i] = [float(x) for x in data[i]]
         return data
 
+def ExtractColummnFromData(matrix, i):
+    return [row[i] for row in matrix]
+
 def main():
     xData = ImportData('xData.csv')
     yData = ImportData('yData.csv')
+    beds = ExtractColummnFromData(xData, 2)
+    plt.plot(beds, yData[0], 'ro')
+    plt.show()
                 
 if __name__ == "__main__":
     main()
