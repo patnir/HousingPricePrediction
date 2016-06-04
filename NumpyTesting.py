16a557999113c530d231440676ce75a84f4292f4
patnir <rahul20patni@gmail.com>
2016-06-04 11:31:36 -0400

    Cleaning up cost function

--- NumpyTesting.py
+++ NumpyTesting.py
@@ -32,23 +32,12 @@ def Plot(x, y, xlabel, ylabel, title):
     plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
-
+    
 def CostFunction(X, y, theta):
     m = len(X)
-    #print X   
-    #print np.size(X)
-    #print np.shape(X)
-    #print np.size(theta)
-    # print np.multiply(X, theta)
-    h_x = []
-    cost = []
-    for i in range(len(X)):
-        h_x.append(sum(X[i] * theta))
-        difference = (1.0 / (2.0 * m)) * (math.pow((h_x[i] - y[i]), 2)) 
-        cost.append(round(difference, 5))
-    J = sum(cost)
-    # print J
-    return J
+    cost = np.array((np.dot(X, theta) - y))
+    result = [(1.0 / (2.0 * m)) * math.pow(x, 2) for x in cost]
+    return sum(result)
     
 def GradientDescent(X, y, theta, alpha):
     temp = theta
@@ -62,6 +51,7 @@ def GradientDescent(X, y, theta, alpha):
             temp[i] = temp[i] - (alpha / m) * (cost) * sum(X[i])
     theta = temp
     
+# def GradientDescent2(X, y, theta, alpha):
 
 def Optimization(X_array, y_array, theta, alpha, repetitions, cost):
     prev_cost = sys.maxint * sys.maxint
@@ -89,23 +79,24 @@ def main():
 #    Plot(beds, y, "regularized beds", "prices", 
 #         "showing relationship between number of beds and price")
     # Converting X into an array using numpy for improved matrix operations
-    X_array = np.array(X)
+    X_array = X
     # Converting y into an array
-    y_array = np.array(y)
+    y_array = y
     # PrintArray(y_array)
     # Assigning zeros to theta to represent initial predictions
     theta = np.zeros(len(X[0]))
     # setting number of repetitions    
     repetitions = 10000
     # setting learning rate
-    alpha = 0.0008
+    alpha = 0.0003
     # initializing costs to keep track of how cost changes with each repetition
     cost = []
     # Optimizing cost function
-    Optimization(X_array, y_array, theta, alpha, repetitions, cost)
+    #Optimization(X_array, y_array, theta, alpha, repetitions, cost)
     # Plotting how cost varies as a function of number of repetitions
-    Plot(range(len(cost)), cost, "repetitions", "cost", "tracking cost as repetitions increase")
-    print "minimum cost = %.4g" %(min(cost))
+    #Plot(range(len(cost)), cost, "repetitions", "cost", "tracking cost as repetitions increase")
+    #print "minimum cost = %.4g" %(min(cost))
+    print CostFunction(X, y, theta)
     
 if __name__ == "__main__":
     main()
